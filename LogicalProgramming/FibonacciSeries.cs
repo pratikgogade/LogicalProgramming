d39e97e505ae5b21d1f35635b6b94270876f18a5
unknown <Pratik@Example.com>
2022-12-09 15:33:11 +0530

    [Pratik]Add:Fibonacci Series

---
+++ LogicalProgramming/FibonacciSeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            Console.WriteLine("Enter The Length Of Series:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int firstNum = 0, secondNum = 1, thirdNum = 0;
+            Console.WriteLine("First no. - " + firstNum + "\n" + "Second no. - " + secondNum);
+            for (int i = 0; i < num; i++)
+            {
+                thirdNum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = thirdNum;
+                Console.WriteLine(thirdNum);
+            }
+        }
+    }
+}
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace LogicalProgramming
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome to Logical Programming Problems");
+                Console.WriteLine("1. Fibonaccis Series" + "\n" + "2. Exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+                switch (check)
+                {
+                    case 1:
+                        FibonacciSeries fibo = new FibonacciSeries();
+                        fibo.Fibonacci();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
